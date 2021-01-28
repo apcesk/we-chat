982f2faae15736d969e178cb0235ca491ef19e87
张玉成 <apcesk@163.com>
2021-01-28 11:51:59 +0800

    --sha1

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const query = require('../app/service/mysql')
+const sha1 = require('sha1');
 // 路由组件
 const Router = require('koa-router');
 const Config = require('../config');
@@ -15,10 +16,22 @@ Routes.get('/', async (ctx) => {
     // let _sql = 'select * from test'
     // const result = await query(_sql)
     // ctx.response.body = result
-    if (ctx.query.signature && ctx.query.timestamp && ctx.query.timestamp && ctx.query.nonce && ctx.query.echostr) {
-        ctx.response.body = ctx.query.echostr
+    console.log(ctx.header.token);
+    if (ctx.query.signature && ctx.query.timestamp && ctx.query.nonce && ctx.query.echostr) {
+        let arr = [ctx.header.token, ctx.query.timestamp, ctx.query.nonce]
+        arr.sort();
+        let str = arr.join('');
+        console.log(str);
+        str = sha1(str);
+        if (ctx.query.signature === str) {
+            // 说明请求来自于微信
+            ctx.response.body = echostr;
+            return;
+        }
+        ctx.response.body = str;
         return;
     }
+    // console.log(ctx)
     ctx.response.body = ctx.query;
 })
 module.exports = Routes;
